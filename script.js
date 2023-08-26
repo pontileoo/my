5c031bda4b59b3a89e286aab9d805ce1e5cac61f
Leoo <140221104+pontileoo@users.noreply.github.com>
2023-08-26 23:37:09 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const sr = ScrollReveal ({
   distance: '65px' ,
-  duration: 2600,
+  duration: 2000,
   delay: 150,
   reset: true
 });
 
 sr.reveal('.container, ul, a',{delay:50, origin:'top'})
 sr.reveal('.bottom',{delay:50, origin:'bottom'})
-sr.reveal('.icons',{delay:50, origin:'left'})
+sr.reveal('.instagram-icon',{delay:150, origin:'left'})
+sr.reveal('.twitter-icon',{delay:350, origin:'left'})
+sr.reveal('.tiktok-icon',{delay:550, origin:'left'})
+sr.reveal('.facebook-icon',{delay:750, origin:'left'})
+
